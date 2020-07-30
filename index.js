72c3506e0585bb5941b4d24ef68059904d3fb151
joshsgt20 <64259172+joshsgt20@users.noreply.github.com>
2020-07-30 20:51:45 +0100

    Addition of Slideshow, Flexbox layout for projects and some HTML text for description of the charity.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ $(document).ready(function(RevealFAQ){
     $("#SmallMenu").slideToggle("slow");
   });
 });
+
+
+///Slideshow JS
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("Slideshow");
+  var Dots = document.getElementsByClassName("Dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < Dots.length; i++) {
+    Dots[i].className = Dots[i].className.replace(" background", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  Dots[slideIndex-1].className += " background";
+  setTimeout(showSlides, 8000);
+}
